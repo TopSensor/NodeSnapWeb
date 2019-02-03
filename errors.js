01d8f3f5c125c884e0f380faa316ea7fc431eed6
bleonard252 <bleonard252@rocketmail.com>
2019-02-03 17:23:40 -0500

    Update errors.js

--- errors.js
+++ errors.js
@@ -3,13 +3,13 @@ var fs = require("fs");
 var util = require("util");
 module.exports.handle = function (error, fullpath) {
   if (error.code == "EISDIR") {
-      if (!util.types.isNativeError(fs.readFileSync(fullpath+"/index.html"))) return fs.readFileSync(fullpath+"/index.html");
+      /*if (!util.types.isNativeError(fs.readFileSync(fullpath+"/index.html"))) return fs.readFileSync(fullpath+"/index.html");
       else if (!util.types.isNativeError(fs.readFileSync(fullpath+"/index.htm"))) return fs.readFileSync(fullpath+"/index.htm");
       else if (!util.types.isNativeError(fs.readFileSync(fullpath+"/default.html"))) return fs.readFileSync(fullpath+"/default.html");
       else if (!util.types.isNativeError(fs.readFileSync(fullpath+"/default.htm"))) return fs.readFileSync(fullpath+"/default.htm");
       else if (!util.types.isNativeError(fs.readFileSync(fullpath+"/.index.js"))) return include("./.index.js");
       else if (!util.types.isNativeError(fs.readFileSync(fullpath+"/.index.js.js"))) return include("./.index.js.js");
-      else {
+      else {*/
       let dir = fs.readdirSync(fullpath);
       let out = "<title>Directory index</title><h1>Directory index</h1><hr /><ul>";
       dir.forEach(function(dirent){
@@ -18,17 +18,22 @@ module.exports.handle = function (error, fullpath) {
       out += `</ul><hr /><center>NodeSnapWeb (NSW); Running on ${process.arch} ${process.platform}; Node version ${process.version}; <br />
       Error code: EISDIR</center>`;
       return out;
-    }}
+    }//}
     else if (error.code == "ENOENT") {
-        return `<title>404 Not Found</title><h2>404 Not Found</h2><hr />
+        return `<title>404 Not Found</title><h2>404 Not Found</h2>
         The file or directory you requested could not be found. Please check the URL or try another name.
         <hr /><center>NodeSnapWeb (NSW); Running on ${process.arch} ${process.platform}; Node version ${process.version}; <br />
-        Error code: ${error.errorCode}</center>`;
+        Error code: ENOENT</center>`;
     }
     else if (error.code == "EPERM") {
-        return `<title>401 Unauthorized</title><h2>401 Unauthorized</h2><hr />
+        return `<title>401 Unauthorized</title><h2>401 Unauthorized</h2>
         You were not granted access to the page you were trying to get to.
         <hr /><center>NodeSnapWeb (NSW); Running on ${process.arch} ${process.platform}; Node version ${process.version}; <br />
-        Error code: ${error.errorCode}</center>`;
+        Error code: EPERM</center>`;
+    }
+    else {
+        return `<title>500 Internal Server Error</title><h2>500 Internal Server Error</h2>
+        <hr /><center>NodeSnapWeb (NSW); Running on ${process.arch} ${process.platform}; Node version ${process.version}; <br />
+        Error code: ${error.code}</center>`;
     }
 }
