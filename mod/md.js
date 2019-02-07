f03670581a1ce6be431da12f79ecabf75886f4b9
bleonard252 <bleonard252@rocketmail.com>
2019-02-07 18:01:16 -0500

    mark it all
    Signed-off-by: bleonard252 <bleonard252@rocketmail.com>

--- mod/md.js
+++ mod/md.js
@@ -11,8 +11,7 @@ for (mdi = 0; mdi < mdx.length; mdi++) {
 };
 
 module.exports.amistad = function (data) {
-  let out = `<!DOCTYPE HTML><head>
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/styles/default.min.css"><script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/highlight.min.js"></script><script>hljs.initHighlightingOnLoad();</script>`; 
+  let out;
   let ssl = `<link rel='stylesheet' href="https://gistcdn.githack.com/bleonard252/3165615ff3d3c8275b33e5eed1841b0b/raw/9dfcafeb5f2373fdb38eef16a32e0d50d6524183/github.css" from-fm-default>`;
   // toc // let datus = mdx.toc.insert(data);
   let front = mdfm(data);
@@ -24,5 +23,8 @@ module.exports.amistad = function (data) {
   else if (front.attributes.style == "none") ssl = "<!-- none from-front-matter -->";
   if (front.attributes.stylesheet != undefined) ssl = `<link rel='stylesheet' href="${front.attributes.stylesheet}" from-front-matter>`;
   out = datae;
-  return out + `<meta content="width=device-width,maximum-scale=1.0,initial-scale=1.0,minimum-scale=1.0,user-scalable=no" name="viewport"><title>${front.attributes.title || "Untitled"}</title>${ssl}</head><body><article>` + datae + `</article></body>`
+  return `<!DOCTYPE HTML><head>
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/styles/default.min.css"><script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/highlight.min.js"></script><script>hljs.initHighlightingOnLoad();</script>
+  <meta content="width=device-width,maximum-scale=1.0,initial-scale=1.0,minimum-scale=1.0,user-scalable=no" name="viewport">
+  <title>${front.attributes.title || "Untitled"}</title>${ssl}</head><body><article>` + datae + `</article></body>`
 }
