cb558a34f02b36731254f84e879b24d9f776c5d1
bleonard252 <bleonard252@rocketmail.com>
2019-02-07 19:03:24 -0500

    gitiles style configurating
    Signed-off-by: bleonard252 <bleonard252@rocketmail.com>

--- mod/md.js
+++ mod/md.js
@@ -11,20 +11,21 @@ for (mdi = 0; mdi < mdx.length; mdi++) {
 };
 
 module.exports.amistad = function (data) {
-  let out;
+  let out; let pfx;
   let ssl = `<link rel='stylesheet' href="https://gistcdn.githack.com/bleonard252/3165615ff3d3c8275b33e5eed1841b0b/raw/9dfcafeb5f2373fdb38eef16a32e0d50d6524183/github.css" from-fm-default>`;
   // toc // let datus = mdx.toc.insert(data);
   let front = mdfm(data);
   data = data.replace(/^---[\s\S]*?---/g,'');
   data = toc.insert(data);
   let datae = md.render(data);
-       if (front.attributes.style == "gitiles") ssl = `<link rel='stylesheet' href="https://gist.githack.com/bleonard252/3165615ff3d3c8275b33e5eed1841b0b/raw/gitiles.css" from-fm-style>`;
+       if (front.attributes.style == "gitiles") ssl = `<link rel='stylesheet' href="https://gistcdn.githack.com/bleonard252/3165615ff3d3c8275b33e5eed1841b0b/raw/79a3058c752cbc6844fedb20789feb813ce4f0f3/gitiles.css" from-fm-style>`;
   else if (front.attributes.style == "github") ssl = ssl;
   else if (front.attributes.style == "none") ssl = "<!-- none from-front-matter -->";
   if (front.attributes.stylesheet != undefined) ssl = `<link rel='stylesheet' href="${front.attributes.stylesheet}" from-front-matter>`;
-  if (front.attributes.style == "gitiles") datae = "<outerwrap><innerwrap>" + datae + "</innerwrap></outerwrap>";
+  if (front.attributes.style == "gitiles") {datae = "<outerwrap><innerwrap>" + datae + "</innerwrap></outerwrap>"; 
+  pfx = `<header class="Site-header"><div class="Header"><div class="Header-title"></div></div></header>` /* add navbar */}
   return `<!DOCTYPE HTML><head>
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/styles/default.min.css"><script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/highlight.min.js"></script><script>hljs.initHighlightingOnLoad();</script>
   <meta content="width=device-width,maximum-scale=1.0,initial-scale=1.0,minimum-scale=1.0,user-scalable=no" name="viewport">
-  <title>${front.attributes.title || "Untitled"}</title>${ssl}</head><body>` + datae + `</body>`
+  <title>${front.attributes.title || "Untitled"}</title>${ssl}</head><body>` + pfx + datae + `</body>`
 }
