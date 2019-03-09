459f417f8246899c0c997c0bf6ddd4ddae402a84
bleonard252 <bleonard252@rocketmail.com>
2019-03-09 15:20:05 -0500

    Added HTTP error codes to Error code field on error pages
    Signed-off-by: bleonard252 <bleonard252@rocketmail.com>

--- mod/errors.js
+++ mod/errors.js
@@ -1,6 +1,6 @@
 //to use: var nsw_errors = require("./mod/errors");
 const fs = require("fs");
-const md = require("./md");
+const md = require("./md.js");
 const indices = ["index.html", "index.htm", "README.md", "readme.html", "readme.htm", "index.md", "default.html", "default.htm", "default.md"];
 module.exports.handle = function (error, fullpath) {
   if (error.code == "EISDIR") {
@@ -16,7 +16,7 @@ module.exports.handle = function (error, fullpath) {
         out += "<li><a href="+dirent+">"+dirent+"</a></li>"
       });
       out += `</ul><hr /><center>NodeSnapWeb (NSW); Running on ${process.arch} ${process.platform}; Node version ${process.version}; <br />
-      Error code: EISDIR</center>`;
+      Error code: 200/EISDIR</center>`;
       return out;}
     else {
       return indexdata;
@@ -25,17 +25,17 @@ module.exports.handle = function (error, fullpath) {
         return `<title>404 Not Found</title><h2>404 Not Found</h2>
         The file or directory you requested could not be found. Please check the URL or try another name.
         <hr /><center>NodeSnapWeb (NSW); Running on ${process.arch} ${process.platform}; Node version ${process.version}; <br />
-        Error code: ENOENT</center>`;
+        Error code: 404/ENOENT</center>`;
     }
     else if (error.code == "EPERM") {
         return `<title>401 Unauthorized</title><h2>401 Unauthorized</h2>
         You were not granted access to the page you were trying to get to.
         <hr /><center>NodeSnapWeb (NSW); Running on ${process.arch} ${process.platform}; Node version ${process.version}; <br />
-        Error code: EPERM</center>`;
+        Error code: 401/EPERM</center>`;
     }
     else {
         return `<title>500 Internal Server Error</title><h2>500 Internal Server Error</h2>
         <hr /><center>NodeSnapWeb (NSW); Running on ${process.arch} ${process.platform}; Node version ${process.version}; <br />
-        Error code: ${error.code}</center>`;
+        Error code: 500/${error.code}</center>`;
     }
 }
