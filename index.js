976bc6b24a6537d6c4285bf406d30ff953f3fd98
bleonard252 <bleonard252@rocketmail.com>
2019-03-10 19:40:22 -0400

    add config sets
    Signed-off-by: bleonard252 <bleonard252@rocketmail.com>

--- index.js
+++ index.js
@@ -12,40 +12,47 @@ var md = require("./mod/md");
 var cmd = process.argv[2];
 
 //config
-var cfgport = (args.port ? args.port : 8080);
-var cfgroot = (args.path ? args.path : (process.env.SNAP_USER_DATA ? process.env.SNAP_USER_DATA : "/var/www/html"));
-  // fallback chain: --path > snap user data folder > /var/www/html
+global['cfgport'] = (args.port ? args.port : 8080);
+	// fallback chain: --port > 8080
+global['cfgroot'] = (args.path ? args.path : (process.env.SNAP_USER_DATA ? process.env.SNAP_USER_DATA : "/var/www/html"));
+	// fallback chain: --path > snap user data folder > /var/www/html
+global['nswcfg'] = {}; require("./mod/nswfile")(); //set config
 
 //action checks
 if (cmd == "start") {
-  //runs from /var/www/html
-  var server = http.createServer();
-  server.listen(cfgport);
-  console.info("HTTP server created successfully at port "+cfgport+" with root dir "+cfgroot);
-  server.on("request",function(request,response){
-    let rqurl = url.parse(request.url);
-    let fullpath = cfgroot + rqurl.pathname;
-    console.log('User accessed ' + rqurl.pathname + "(" + fullpath + " on the system)");
-    fs.readFile(fullpath, (err, data) => {
-      if (err) {console.error(err);
-      if (err.code == "ENOENT") { //404 Not Found
-        response.statusCode = 404;
-        if (fullpath.endsWith('.html')) {try {let fi = fs.readFileSync(fullpath.replace(/\.html$/,'.md'));response.end(md.amistad(fi.toString()))} catch(e) {response.end(nsw_errors.handle(e,fullpath))}}
-        else response.end(nsw_errors.handle(err,fullpath));
-      } else if (err.code == "EISDIR") {
-        if (rqurl.pathname.charAt(rqurl.pathname.length - 1) != "/") {response.setHeader("Location",rqurl.pathname+"/");response.statusCode = 301;response.end()}
-        else {
-        response.statusCode = 200;
-        response.end(nsw_errors.handle(err,fullpath));}
-      } else if (err.code == "EPERM") {
-        response.statusCode = 401;
-        response.end(nsw_errors.handle(err,fullpath));
-      }} else {
-        response.statusCode = 200;
-        if (fullpath.endsWith('.md')) response.end(md.amistad(data.toString()));
-        else response.end(data);
-    }});})
+	//runs from /var/www/html
+	var server = http.createServer();
+	server.listen(cfgport);
+	console.info("HTTP server created successfully at port " + cfgport + " with root dir " + cfgroot);
+	server.on("request", function (request, response) {
+		let rqurl = url.parse(request.url);
+		let fullpath = cfgroot + rqurl.pathname;
+		console.log('User accessed ' + rqurl.pathname + "(" + fullpath + " on the system)");
+		fs.readFile(fullpath, (err, data) => {
+			if (err) {
+				console.error(err);
+				if (err.code == "ENOENT") { //404 Not Found
+					response.statusCode = 404;
+					if (fullpath.endsWith('.html')) { try { let fi = fs.readFileSync(fullpath.replace(/\.html$/, '.md')); response.end(md.amistad(fi.toString())) } catch (e) { response.end(nsw_errors.handle(e, fullpath)) } }
+					else response.end(nsw_errors.handle(err, fullpath));
+				} else if (err.code == "EISDIR") {
+					if (rqurl.pathname.charAt(rqurl.pathname.length - 1) != "/") { response.setHeader("Location", rqurl.pathname + "/"); response.statusCode = 301; response.end() }
+					else {
+						response.statusCode = 200;
+						response.end(nsw_errors.handle(err, fullpath));
+					}
+				} else if (err.code == "EPERM") {
+					response.statusCode = 401;
+					response.end(nsw_errors.handle(err, fullpath));
+				}
+			} else {
+				response.statusCode = 200;
+				if (fullpath.endsWith('.md')) response.end(md.amistad(data.toString()));
+				else response.end(data);
+			}
+		});
+	})
 };
 if (cmd == "run") {
-  include(args.file);
+	include(args.file);
 }
--- mod/nswfile.js
+++ mod/nswfile.js
@@ -1,2 +1,23 @@
 //This mod will handle .nsw config files.
 //NodeSnapWeb's .nsw files will be YAML formatted and documented on the Wiki.
+const yml = require("js-yaml"); //this is a dependency of front-matter so it should be installed for that
+const fs = require("fs");
+
+module.exports = function() {
+	let defaults =
+	`site:
+		name: null
+	md:
+		style: github
+	listing: on
+	indexes: on
+	`
+	let changes = "";
+	try {changes = fs.readFileSync(cfgroot+"/.nsw");}
+	catch(e) {
+		try{changes = fs.readFileSync(cfgroot+"/nsw.cfg");} //if your fs doesn't allow hidden files
+		catch(f) {changes = "";}
+	}
+
+	nswcfg = yml.safeLoad(defaults + changes);
+}
