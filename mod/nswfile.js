7506c18c2fc2667acbb74c7c1c426781722a7c27
bleonard252 <bleonard252@rocketmail.com>
2019-05-05 16:57:51 -0400

    hey! you forgot something!
    Signed-off-by: bleonard252 <bleonard252@rocketmail.com>

--- mod/nswfile.js
+++ mod/nswfile.js
@@ -1,18 +1,26 @@
-//This mod will handle .nsw config files.
-//NodeSnapWeb's .nsw files will be YAML formatted and documented on the Wiki.
+/**
+ * This mod will handle .nsw config files. 
+ * NodeSnapWeb's .nsw files will be YAML formatted and documented on the Wiki.
+ */
 const yml = require("js-yaml"); //this is a dependency of front-matter so it should be installed for that
 const fs = require("fs");
 const merge = require("lodash.merge");
 
-module.exports = function() {
+/**
+ * @description Sets the value of the NSW global `nswcfg`.
+ * @param {boolean} is Is the NSW file being refreshed? If so, this function will not log its warnings about nsw.cfg.
+ * Also when doing this, NSW may refresh other files in the background.
+ */
+module.exports = function(is) { 
+	var is = is || false;
 	var defaults = fs.readFileSync(process.cwd()+'/nswdefaults.yaml') + "";
 	var changes = "";
 	try {changes = fs.readFileSync(cfgroot+"/.nsw");}
 	catch(e) {
-		try{console.warn("Using nsw.cfg exposes your website's settings to the Internet! It is recommended that you use a .nsw file if you can instead.");
+		try{if (!is) console.warn("Using nsw.cfg exposes your website's settings to the Internet! It is recommended that you use a .nsw file if you can instead.");
 			changes = fs.readFileSync(cfgroot+"/nsw.cfg");
-			console.warn("Used nsw.cfg " + e)} //if your fs doesn't allow hidden files. nsw shows this, so use .nsw where you can
-		catch(f) {changes = "";console.warn("No changes file " + f)}
+			if (!is) console.warn("Used nsw.cfg")} //if your fs doesn't allow hidden files. nsw shows this, so use .nsw where you can
+		catch(f) {changes = ""; if (!is) console.warn("No changes file " + f)}
 	}
 
 	let defyml = yml.safeLoad(defaults.toString());
