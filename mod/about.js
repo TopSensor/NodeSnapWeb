398345f693d6e437ad3457ea91169a44d432613a
bleonard252 <bleonard252@rocketmail.com>
2019-05-06 20:52:35 -0400

    add todos?
    Signed-off-by: bleonard252 <bleonard252@rocketmail.com>

---
+++ mod/about.js
@@ -0,0 +1,7 @@
+var medicine;
+var medicinebasic = (basic) => medicine = html`<!DOCTYPE html>
+<head>
+    <title>About NodeSnapWeb</title>
+    <link rel="stylesheet" href="" /><!--TODO: add endo.css-->
+    <!--TODO: add endo customizations-->
+</head>`;
